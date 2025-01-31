21d748a707af499fe3cdf63b4316efd4a4ae5cd6
OBud <thomas.menes@hotmail.com>
2025-01-31 08:40:04 +0100

    webproject

--- web_project/env.sh
+++ web_project/env.sh
@@ -1,5 +1,10 @@
-. conf/$env.sh
-export BASE_FOLDER=$(pwd)
-export DB_PATH="$BASE_FOLDER/$DB_NAME"
+export BASE_DIR=$(pwd)
+
+. $BASE_DIR/conf/$env.sh
+. $BASE_DIR/venv/bin/activate
+
+export APP_DIR="$BASE_DIR/app"
+export TEMPLATE_DIR="$APP_DIR/templates"
+export DB_PATH="$BASE_DIR/$DB_NAME"
 
 alias sql3="sqlite3 $DB_NAME"
